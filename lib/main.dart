a0ab49223ea96d2bc6c2791a62f0a228e977eb9f
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-02 11:57:51 +0700

    feat: add ListView.builder for startup name word pairs

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -36,9 +29,48 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /* 1 */ (context, i) {
+        if (i.isOdd) return const Divider(); /* 2 */
+
+        final index = i ~/ 2; /* 3 */
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /* 4 */
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 100.0,
+            child: Text('Test layout of column'),
+          ),
+          Expanded(child: _buildSuggestions()),
+        ],
+      ),
+    );
   }
 }
